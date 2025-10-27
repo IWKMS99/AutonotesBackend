59b0eb542f4724daa84c95bf73a8a38c467317a9
IWKMS99 <alievsaid0909@yandex.ru>
2025-10-27 18:56:12 +0300

    feat(security): настроена базовая конфигурация UserDetailsService и AuthenticationProvider

---
+++ src/main/java/ru/mtuci/autonotesbackend/security/SecurityUser.java
@@ -0,0 +1,27 @@
+package ru.mtuci.autonotesbackend.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.mtuci.autonotesbackend.modules.user.impl.domain.User;
+
+import java.util.Collection;
+import java.util.List;
+
+public record SecurityUser(User user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}

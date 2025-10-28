9178fb4b5051f0c744d5ff275039b24f4ef91b1f
IWKMS99 <alievsaid0909@yandex.ru>
2025-10-28 12:23:22 +0300

    feat(notes): реализован сервисный слой для создания конспектов

---
+++ src/main/java/ru/mtuci/autonotesbackend/modules/notes/impl/service/NoteService.java
@@ -0,0 +1,34 @@
+package ru.mtuci.autonotesbackend.modules.notes.impl.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import ru.mtuci.autonotesbackend.modules.notes.impl.domain.LectureNote;
+import ru.mtuci.autonotesbackend.modules.notes.impl.domain.NoteStatus;
+import ru.mtuci.autonotesbackend.modules.notes.impl.repository.LectureNoteRepository;
+import ru.mtuci.autonotesbackend.modules.user.impl.domain.User;
+import ru.mtuci.autonotesbackend.modules.user.impl.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class NoteService {
+
+    private final LectureNoteRepository noteRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public LectureNote createNote(String title, MultipartFile file, String filePath, Long userId) {
+        User user = userRepository.getReferenceById(userId);
+
+        LectureNote note = LectureNote.builder()
+                .title(title)
+                .user(user)
+                .originalFileName(file.getOriginalFilename())
+                .fileStoragePath(filePath)
+                .status(NoteStatus.PROCESSING)
+                .build();
+
+        return noteRepository.save(note);
+    }
+}

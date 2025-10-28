c5bb70835d7200c56273b448b68ee301a4e17537
IWKMS99 <alievsaid0909@yandex.ru>
2025-10-28 14:25:31 +0300

    feat(notes): добавлен слой данных для получения детальной информации о конспекте

--- src/main/java/ru/mtuci/autonotesbackend/modules/notes/impl/mapper/NoteMapper.java
+++ src/main/java/ru/mtuci/autonotesbackend/modules/notes/impl/mapper/NoteMapper.java
@@ -3,6 +3,7 @@ package ru.mtuci.autonotesbackend.modules.notes.impl.mapper;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import ru.mtuci.autonotesbackend.modules.notes.api.dto.NoteDetailDto;
 import ru.mtuci.autonotesbackend.modules.notes.api.dto.NoteDto;
 import ru.mtuci.autonotesbackend.modules.notes.impl.domain.LectureNote;
 
@@ -12,4 +13,7 @@ public interface NoteMapper {
     NoteDto toDto(LectureNote lectureNote);
 
     List<NoteDto> toDtoList(List<LectureNote> notes);
+
+    @Mapping(source = "user.id", target = "userId")
+    NoteDetailDto toDetailDto(LectureNote lectureNote);
 }
--- src/main/java/ru/mtuci/autonotesbackend/modules/notes/impl/repository/LectureNoteRepository.java
+++ src/main/java/ru/mtuci/autonotesbackend/modules/notes/impl/repository/LectureNoteRepository.java
@@ -1,6 +1,7 @@
 package ru.mtuci.autonotesbackend.modules.notes.impl.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.mtuci.autonotesbackend.modules.notes.impl.domain.LectureNote;
@@ -9,4 +10,6 @@ import ru.mtuci.autonotesbackend.modules.notes.impl.domain.LectureNote;
 public interface LectureNoteRepository extends JpaRepository<LectureNote, Long> {
 
     List<LectureNote> findByUserId(Long userId);
+
+    Optional<LectureNote> findByIdAndUserId(Long id, Long userId);
 }
